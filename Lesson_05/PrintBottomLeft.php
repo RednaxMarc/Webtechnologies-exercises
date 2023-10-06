1f7e7e65eca6d0850c8af846a705ccfec6a9b903
RednaxMarc <xander.marcle@hotmail.com>
2023-10-06 12:27:56 +0200

    added extra exercises

---
+++ Lesson_05/PrintBottomLeft.php
@@ -0,0 +1,13 @@
+<?php
+    $width = 
+    $height = $argv[1];
+    $mylist = range (1,$argv[1]);
+    
+    for (isset($argv[1]) == TRUE; $height > 0; $height -= 0){
+        while($width != $argv[1]){
+            echo "*";
+            $width
+        }
+        
+    }
+?>
--- Lesson_05/SquareOfAsterisks.php
+++ Lesson_05/SquareOfAsterisks.php
@@ -1,8 +1,30 @@
 <?php
-    for (isset($argv[1]) == TRUE; $argv[1] > 0){
-        for ($argv[1] > 0; $argv[1] -= 1){
-            echo "*";
+    if (isset($argv[1]) == TRUE AND isset($argv[2]) == FALSE){
+        $columns = $argv[1];
+        $rows = $argv[1];
+
+        for (isset($argv[1]) == TRUE; $rows > 0; $rows -= 1){
+            while ($columns > 0){
+                echo "*";
+                $columns -= 1;      
+            }
+            $columns = $argv[1];
+            echo "\n";
         }
-        echo "\n"; 
     }
+    else if (isset($argv[1]) == TRUE AND isset($argv[2]) == TRUE){
+        $columns = $argv[1];
+        $rows = $argv[2];
+
+        for (isset($argv[2]) == TRUE; $rows > 0; $rows -= 1){
+            while ($columns > 0){
+                echo "*";
+                $columns -= 1;      
+            }
+            $columns = $argv[1];
+            echo "\n";
+        }
+    }    
 ?>
+    
+    
