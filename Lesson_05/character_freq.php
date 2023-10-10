14f3e2d47fe2714e81d21f861ddbd5b8adc97402
RednaxMarc <xander.marcle@hotmail.com>
2023-10-10 10:57:03 +0200

    ...

---
+++ Lesson_05/character_freq.php
@@ -0,0 +1,29 @@
+<?php
+    $array = str_split($argv[1]);
+
+    echo "input: $argv[1] \n\n";
+
+    $total = count($array);
+    $occurences = array_count_values($array);
+    echo "STATS";
+    foreach ($occurences as $character => $times){
+        $prev = $times/$total *100;
+        echo "$character: $times occurences -> $prev %\n";
+    }
+    echo "\n";
+
+    echo "Order by frequency: \n";
+    asort($occurences);
+    foreach ($occurences as $character => $times){
+        $prev = $times/$total *100;
+        echo "$character: $times occurences -> $prev %\n";
+    }
+    echo "\n";
+
+    echo "Order by character: \n";
+    ksort($occurences);
+    foreach ($occurences as $character => $times){
+        $prev = $times/$total *100;
+        echo "$character: $times occurences -> $prev %\n";
+    }
+?>
