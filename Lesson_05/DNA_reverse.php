495f4f848b0664a99d8f17cb90ba149307dd5113
RednaxMarc <xander.marcle@hotmail.com>
2023-10-10 11:39:54 +0200

    ...

--- Lesson_05/DNA_reverse.php
+++ Lesson_05/DNA_reverse.php
@@ -1,11 +1,13 @@
 <?php
     $FWD_strand = str_split($argv[1]);
     #print_r($FWD_strand);
-    echo "$argv[1] \n";
+    echo "orig.:$argv[1] \n";
+    echo "      ";
     foreach ($FWD_strand as $nt){
         echo "|";
     }
     echo "\n";
+    echo "comp.:";
     foreach ($FWD_strand as $nt){
         if ($nt == "A"){
             echo "T";
